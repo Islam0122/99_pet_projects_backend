8b15877a0b9c1184220439a70c28fe3bf0464ceb
islam0122 <duishobaevislam01@gmail.com>
2025-09-16 00:36:19 +0600

    12 --> EnglishLevelTest_DRF_REACT_12_project --> add -> doker

--- EnglishLevelTest_DRF_REACT_12_project/backend/config/settings/production.py
+++ EnglishLevelTest_DRF_REACT_12_project/backend/config/settings/production.py
@@ -7,15 +7,10 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Безопасность
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
