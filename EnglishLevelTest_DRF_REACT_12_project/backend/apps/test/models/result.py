494f45f9ec152fa294b9239c92f54104c9e7bab6
islam0122 <duishobaevislam01@gmail.com>
2025-09-17 01:28:40 +0600

    12 --> EnglishLevelTest_DRF_REACT_12_project --> add -> doker

--- EnglishLevelTest_DRF_REACT_12_project/backend/apps/test/models/result.py
+++ EnglishLevelTest_DRF_REACT_12_project/backend/apps/test/models/result.py
@@ -44,62 +44,7 @@ class ResultsTest(BaseModel):
         verbose_name_plural = 'Результаты тестов'
         ordering = ['-created_at']
 
-# ===========================
-# Сигнал для генерации сертификата и отправки письма
-# ===========================
-@receiver(post_save, sender=ResultsTest)
-def generate_certificate_and_send_email(sender, instance, created, **kwargs):
-    if created and not instance.certificate:
-        context = {
-            'name': instance.name,
-            'topic': instance.test.name,
-            'level': instance.test.level,
-            'percentage': instance.percentage,
-            'correct_answers': instance.correct_answers,
-            'total_questions': instance.total_questions,
-            'date': instance.created_at.strftime('%d-%m-%Y'),
-        }
 
-        html_string = render_to_string("certificate_template.html", context)
-
-        pdf_file = BytesIO()
-        base_url = f'file://{os.path.join(settings.BASE_DIR, "core", "static")}/'
-        HTML(string=html_string, base_url=base_url).write_pdf(pdf_file)
-
-        pdf_file.seek(0)
-        filename = f"certificate_{instance.id}.pdf"
-        pdf_content = pdf_file.read()
-        instance.certificate.save(filename, ContentFile(pdf_content))
-
-        email_subject = '🎓 Ваш сертификат за прохождение теста по английскому'
-        email_body = f"""
-Здравствуйте, {instance.name}!
-
-Поздравляем с успешным завершением теста по теме "{instance.test.name}" ({instance.test.level})! 🎉
-
-Результаты:
-- Правильных ответов: {instance.correct_answers}
-- Неправильных ответов: {instance.wrong_answers}
-- Процент правильных ответов: {instance.percentage}%
-
-Ваш сертификат прикреплён к письму. Скачайте его и сохраняйте!
-
-С уважением Islam dev,
-Команда обучения английскому 🌟
-"""
-        try:
-            email = EmailMessage(
-                subject=email_subject,
-                body=email_body,
-                from_email=settings.EMAIL_HOST_USER,
-                to=[instance.email],
-            )
-            email.attach(filename, pdf_content, 'application/pdf')
-            email.send()
-        except Exception as e:
-            logger.error(f"Ошибка при отправке письма: {e}")
-
-        pdf_file.close()
 
 
 # ===========================
