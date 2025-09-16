71219a84c9d1a72a723d4289ee294c8e8f1deb89
islam0122 <duishobaevislam01@gmail.com>
2025-09-17 00:56:35 +0600

    12 --> EnglishLevelTest_DRF_REACT_12_project --> add -> doker

--- EnglishLevelTest_DRF_REACT_12_project/backend/config/settings/celery.py
+++ EnglishLevelTest_DRF_REACT_12_project/backend/config/settings/celery.py
@@ -1,13 +1,8 @@
 import os
 from celery import Celery
 
-# указываем Django настройки
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 app = Celery('config')
-
-# читаем настройки CELERY_* из settings.py
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# автоматически подключаем все tasks.py
 app.autodiscover_tasks()
