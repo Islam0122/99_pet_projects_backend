031393134f45441d8a7471fe5182bf8b88c641d3
islam0122 <duishobaevislam01@gmail.com>
2025-09-22 00:00:14 +0600

    13 --> RecipeAppAI_API_(DRF+Aiogram)_13_project

--- RecipeAppAI_API_(DRF + Aiogram)_13_project/backend/apps/user/views.py
+++ RecipeAppAI_API_(DRF + Aiogram)_13_project/backend/apps/user/views.py
@@ -5,7 +5,16 @@ from .serializers import TgUserSerializer
 class TgUserViewSet(viewsets.ModelViewSet):
     queryset = TgUser.objects.all()
     serializer_class = TgUserSerializer
+
+    # Поддержка поиска и сортировки
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["username", "full_name", "telegram_id"]
+    search_fields = ["username", "full_name"]
     ordering_fields = ["created_at", "username"]
     ordering = ["-created_at"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        telegram_id = self.request.query_params.get("telegram_id")
+        if telegram_id:
+            queryset = queryset.filter(telegram_id=telegram_id)
+        return queryset
