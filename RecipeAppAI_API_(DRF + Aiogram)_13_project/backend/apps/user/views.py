958c5a63704b455814205e5f5f2c0913fbddda09
islam0122 <duishobaevislam01@gmail.com>
2025-09-28 00:08:31 +0600

    13 --> RecipeAppAI_API_(DRF+Aiogram)_13_project

--- RecipeAppAI_API_(DRF + Aiogram)_13_project/backend/apps/user/views.py
+++ RecipeAppAI_API_(DRF + Aiogram)_13_project/backend/apps/user/views.py
@@ -1,20 +1,34 @@
 from rest_framework import viewsets, filters
 from .models import TgUser
 from .serializers import TgUserSerializer
+from django.core.cache import cache
 
 class TgUserViewSet(viewsets.ModelViewSet):
     queryset = TgUser.objects.all()
     serializer_class = TgUserSerializer
 
-    # Поддержка поиска и сортировки
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ["username", "full_name"]
     ordering_fields = ["created_at", "username"]
     ordering = ["-created_at"]
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         telegram_id = self.request.query_params.get("telegram_id")
         if telegram_id:
-            queryset = queryset.filter(telegram_id=telegram_id)
-        return queryset
+            cache_key = f"tg_user:{telegram_id}"
+            user = cache.get(cache_key)
+            if user:
+                return TgUser.objects.filter(id=user['id'])
+
+            queryset = super().get_queryset().filter(telegram_id=telegram_id)
+            if queryset.exists():
+                user_data = {
+                    'id': queryset.first().id,
+                    'username': queryset.first().username,
+                    'full_name': queryset.first().full_name,
+                    'telegram_id': queryset.first().telegram_id,
+                }
+                cache.set(cache_key, user_data, timeout=60)  # кэш на 60 секунд
+            return queryset
+
+        return super().get_queryset()
