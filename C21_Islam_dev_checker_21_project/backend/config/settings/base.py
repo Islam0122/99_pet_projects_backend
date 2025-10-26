f6b56695c33992ecbc0b08e0afa063767e5c14da
islam0122 <duishobaevislam01@gmail.com>
2025-10-27 03:45:09 +0600

    C21_Islam_dev_checker_21_project

--- C21_Islam_dev_checker_21_project/backend/config/settings/base.py
+++ C21_Islam_dev_checker_21_project/backend/config/settings/base.py
@@ -70,23 +70,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # # --- База данных (PostgreSQL) ---
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST', default='localhost'),
-#         'PORT': env('DB_PORT', default='5432'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default='5432'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # --- Локализация ---
 LANGUAGE_CODE = 'ru-RU'
 TIME_ZONE = 'Asia/Almaty'
