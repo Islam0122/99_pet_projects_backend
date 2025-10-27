6225b4570eb9970e5034e36fb5d30f66edc79944
islam0122 <duishobaevislam01@gmail.com>
2025-10-27 13:44:28 +0600

    C21_Islam_dev_checker_21_project

--- B20_Readify_20_project/backend/config/settings/development.py
+++ B20_Readify_20_project/backend/config/settings/development.py
@@ -14,11 +14,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': os.environ.get('PGHOST', 'localhost'),
+        'PORT': os.environ.get('PGPORT', '5432'),
     }
 }
 
