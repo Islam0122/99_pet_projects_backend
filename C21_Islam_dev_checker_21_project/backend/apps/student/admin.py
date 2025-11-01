94312e2ed04a8b961d7ffbaf7bc229e7f5af313e
islam0122 <duishobaevislam01@gmail.com>
2025-11-02 00:45:47 +0600

    C21_Islam_dev_checker_21_project

--- C21_Islam_dev_checker_21_project/backend/apps/student/admin.py
+++ C21_Islam_dev_checker_21_project/backend/apps/student/admin.py
@@ -1,5 +1,62 @@
 from django.contrib import admin
 from .models import Group, Student
+from django.contrib import admin
+from django.utils.html import format_html
+from ..homework.models import  Month1Homework, Month1HomeworkItem, Month2Homework, Month3Homework
+
+
+# ===== Month 1 =====
+class Month1HomeworkItemInline(admin.TabularInline):
+    model = Month1HomeworkItem
+    extra = 0
+    fields = ("task_condition", "student_answer", "grade", "is_checked_colored", "ai_feedback", "originality_check")
+    readonly_fields = ("ai_feedback", "originality_check", "is_checked_colored")
+    show_change_link = True
+
+    def is_checked_colored(self, obj):
+        color = "green" if obj.is_checked else "red"
+        text = "Проверено" if obj.is_checked else "Не проверено"
+        return format_html('<b><span style="color: {};">{}</span></b>', color, text)
+
+    is_checked_colored.short_description = "Статус проверки"
+
+
+class Month1HomeworkInline(admin.TabularInline):
+    model = Month1Homework
+    extra = 0
+    fields = ("lesson", "created_at")
+    readonly_fields = ("lesson", "created_at")
+    inlines = [Month1HomeworkItemInline]
+
+
+# ===== Month 2 =====
+class Month2HomeworkInline(admin.TabularInline):
+    model = Month2Homework
+    extra = 0
+    fields = ("title", "lesson", "grade", "is_checked_colored", "github_url", "created_at")
+    readonly_fields = ("grade", "is_checked_colored", "created_at")
+
+    def is_checked_colored(self, obj):
+        color = "green" if obj.is_checked else "red"
+        text = "Проверено" if obj.is_checked else "Не проверено"
+        return format_html('<b><span style="color: {};">{}</span></b>', color, text)
+
+    is_checked_colored.short_description = "Статус проверки"
+
+
+# ===== Month 3 =====
+class Month3HomeworkInline(admin.TabularInline):
+    model = Month3Homework
+    extra = 0
+    fields = ("title", "lesson", "grade", "is_checked_colored", "github_url", "created_at")
+    readonly_fields = ("grade", "is_checked_colored", "created_at")
+
+    def is_checked_colored(self, obj):
+        color = "green" if obj.is_checked else "red"
+        text = "Проверено" if obj.is_checked else "Не проверено"
+        return format_html('<b><span style="color: {};">{}</span></b>', color, text)
+
+    is_checked_colored.short_description = "Статус проверки"
 
 
 @admin.register(Group)
@@ -55,7 +112,10 @@ class StudentAdmin(admin.ModelAdmin):
             ),
             "description": "Учебная статистика и прогресс студента."
         }),
+
     )
+    inlines = [Month1HomeworkInline, Month2HomeworkInline, Month3HomeworkInline]
+
 
     readonly_fields = (
         "average_score",
